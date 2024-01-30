b8b60653c792e3bf5bd9d55bbf01c720d5b167ec
Aarti <143300846+aartijangra@users.noreply.github.com>
2024-01-30 21:42:30 +0530

    Time: 6 ms (48.99%), Space: 9 MB (8.30%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string ans;
+        for(int i=0; i<s.size(); i++){
+            if(isalnum(s[i])){
+                ans.push_back(tolower(s[i]));
+            }
+        }
+        int start=0;
+        int end=ans.size()-1;
+        while(start<=end){
+            if(ans[start]!=ans[end]){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+};
