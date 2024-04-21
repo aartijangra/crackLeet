ee17db445e7b1ea047925155ca8452722db79063
Aarti <143300846+aartijangra@users.noreply.github.com>
2024-04-21 13:30:53 +0530

    Time: 35 ms (32.64%), Space: 31.7 MB (39.62%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
@@ -5,21 +5,23 @@ public:
         int n2 = grid.size() * grid.size();
         unordered_set<int> seen;
 
-        // find the duplicate integer `a`
-        int a;
-        for (auto& row : grid) {
-            for (auto& num : row) {
-                if (seen.count(num) != 0) a = num;
-                seen.insert(num);
+        // find the duplicate integer `repeat`
+        int repeat;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                if (seen.count(grid[i][j]) != 0) 
+                    repeat = grid[i][j];
+                seen.insert(grid[i][j]);
             }
         }
         
-        // find the missing integer `b`
-        int b;
+        // find the missing integer `miss`
+        int miss;
         for (int num = 1; num <= n2; num++) {
-            if (seen.count(num) == 0) b = num;
+            if (seen.count(num) == 0) 
+                miss = num;
         }
         
-        return {a, b};
+        return {repeat, miss};
     }
 };
