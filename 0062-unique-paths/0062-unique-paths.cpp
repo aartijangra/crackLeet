dfc7a97e86dd7887619826ee8507eda5249be526
Aarti <143300846+aartijangra@users.noreply.github.com>
2024-04-22 23:43:34 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (86.23%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        long ans = 1;
+        long ans = 1;  
         for(int i = m+n-2, j = 1; i >= max(m, n); i--, j++) 
             ans = (ans * i) / j;
         return ans;
