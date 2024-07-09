bb6a58c788216a55b4f05d305bea9ae371b9e0bf
Aarti <143300846+aartijangra@users.noreply.github.com>
2024-07-09 21:26:10 +0530

    Time: 38 ms (33.44%), Space: 38.2 MB (30.33%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -3,19 +3,15 @@ public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
         int n = nums.size();
         unordered_map<int,int> mpp;
-        int cnt = 0;
-        int sum = 0;
-        
+        int cnt=0; int sum=0;
         for(int i=0; i<n; i++){
             sum+=nums[i];
             if(sum==goal) cnt++;
-            
             if(mpp.find(sum-goal) != mpp.end()){
                 cnt+=mpp[sum-goal];
             }
             mpp[sum]++;
         }
-        
         return cnt;
     }
 };
