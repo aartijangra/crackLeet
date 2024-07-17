ad64777ea8eba82f78c941a6fe2b7dae7dfbc5e1
Aarti <143300846+aartijangra@users.noreply.github.com>
2024-07-17 19:41:09 +0530

    Time: 0 ms (100.00%), Space: 12.9 MB (34.23%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,50 +1,26 @@
 class Solution {
-
 public:
-
-void findCombination(int index,int target,vector<int>&ds,vector<vector<int>>&ans,vector<int>arr,int n){
-
-    //Base Condition:
-    if(target==0){
-
-        ans.push_back(ds);
-         return;
+    void func(int ind, vector<int>& candidates, int target, vector<int>& ds, vector<vector<int>>& ans){
+        if(target==0){
+            ans.push_back(ds);
+            return;
+        }
+        
+        for(int i=ind; i<candidates.size(); i++){
+            if(candidates[i] > target) break;
+            if(i>ind && candidates[i]==candidates[i-1]) continue;
+            ds.push_back(candidates[i]);
+            func(i+1, candidates, target-candidates[i], ds, ans);
+            ds.pop_back();
+        }
+        
+        
     }
-   
-   
-   
-  for(int i=index;i<n;i++){
-
-    if((i>index) and  (arr[i]==arr[i-1])){
-        continue;
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<int> ds;
+        vector<vector<int>> ans;
+        func(0,candidates,target,ds,ans);
+        return ans;
     }
-
-    if(arr[i]>target){
-       break;
-    }
-    ds.push_back(arr[i]);
-
-    findCombination(i+1,target-arr[i],ds,ans,arr,n);
-
-    ds.pop_back();
-}
-
-
-}
-
-
-vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-      //Here using the same index is not allowed
-      int n=candidates.size();
-      vector<vector<int>>ans;
-      
-      vector<int>ds;
-
-      sort(candidates.begin(),candidates.end());
-
-      findCombination(0,target,ds,ans,candidates,n); 
-
-      return ans; 
-    }
-    
 };
