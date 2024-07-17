b6a11352bedcfaa7fc6315d0baa4e39451fdaae4
Aarti <143300846+aartijangra@users.noreply.github.com>
2024-07-17 21:59:22 +0530

    Time: 92 ms (48.95%), Space: 61.3 MB (37.56%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    vector<vector<string>> res;
-    bool pal(int i,int j,string &s)
-    {
-        while(i<j)
-        {
-            if(s[i]!=s[j])
-                return false;
-            i++;j--;
+    bool isPalindrome(string s, int start, int end){
+        while(start<=end){
+            if(s[start++] != s[end--]) return false;  
         }
         return true;
     }
-    void f(int i,string &s,vector<string> ans)
-    {
-        if(i>=s.size())
-        {
-            if(ans.size()>0)
-               res.push_back(ans);
+    
+    void func(int ind, string s, vector<string>& ds, vector<vector<string>>& ans){
+        //base case
+        if(ind == s.size()){
+            ans.push_back(ds);
             return;
         }
-        for(int k=i;k<s.size();k++)
-        {
-            if(pal(i,k,s))
-            {
-                ans.push_back(s.substr(i,k-i+1));
-                f(k+1,s,ans);
-                ans.pop_back();
+        
+        for(int i=ind; i<s.size(); i++){
+            if(isPalindrome(s,ind,i)){
+                ds.push_back(s.substr(ind, i-ind+1));
+                func(i+1, s, ds, ans);
+                ds.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
-        vector<string> ans;
-        f(0,s,ans);
-        return res;
+        vector<vector<string>> ans;
+        vector<string> ds;
+        func(0, s, ds, ans);
+        return ans;
     }
 };
