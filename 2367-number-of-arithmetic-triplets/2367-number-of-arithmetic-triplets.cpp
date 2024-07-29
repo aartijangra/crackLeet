afb1ca7502a237ea81c78f344e96b074c1500deb
Aarti <143300846+aartijangra@users.noreply.github.com>
2024-07-29 23:58:32 +0530

    Time: 18 ms (30.32%), Space: 10.7 MB (52.30%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-    int arithmeticTriplets(vector<int>& nums, int diff) {
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
         int n = nums.size();
-        int count=0;
+        vector<vector<int>> ans;
         for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                int diff1 = nums[j] - nums[i];
-                for(int k=j+1; k<n; k++){
-                    int diff2 = nums[k] - nums[j];
-                    if(diff1 == diff && diff2 == diff) count++;
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            
+            int j = i+1;
+            int k = n-1;
+            
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum>0) k--;
+                else if(sum<0) j++;
+                else{
+                    vector<int> temp = {nums[i], nums[j], nums[k]};
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
                 }
             }
         }
-        return count;
+        return ans;
     }
 };
