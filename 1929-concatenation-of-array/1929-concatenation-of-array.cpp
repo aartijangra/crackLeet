14c784d6728ba1194db2ec0c3031f5052c9281fc
Aarti <143300846+aartijangra@users.noreply.github.com>
2024-07-30 23:39:07 +0530

    Time: 5 ms (59.24%), Space: 15 MB (64.19%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        vector<int> ans;
         int n = nums.size();
+        vector<int> ans(2*n);
+        
         for(int i=0; i<n; i++){
-            ans.push_back(nums[i]);
-        }
-        for(int i=0; i<n; i++){
-            ans.push_back(nums[i]);
+            ans[i] = ans[i+n] = nums[i];
         }
         return ans;
     }
