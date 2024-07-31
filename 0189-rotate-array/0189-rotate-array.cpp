7f07c6ff9c61f7cb8a914f42639c5906e601cbec
Aarti <143300846+aartijangra@users.noreply.github.com>
2024-08-01 01:50:36 +0530

    Time: 26 ms (16.45%), Space: 28.2 MB (14.60%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -4,10 +4,8 @@ public:
         int n = nums.size();
         vector<int> temp(n);
         for(int i=0; i<n; i++){
-            
-            temp[(i+k)%n]=nums[i];
+            temp[(i+k)%n] = nums[i];
         }
-        
-        nums=temp;
+        nums = temp;
     }
 };
