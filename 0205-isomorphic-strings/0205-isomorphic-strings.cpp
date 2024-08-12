ec64c27bed5f993faf977f972a70fef836108a19
Aarti <143300846+aartijangra@users.noreply.github.com>
2024-08-12 11:49:05 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (69.18%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        vector<int> indexS(200, 0);
-        vector<int> indexT(200, 0); 
+        vector<int> indexS(200,0);
+        vector<int> indexT(200,0);
         
-        int len = s.length(); 
-        
-        if(len != t.length()) { 
-            return false;
+        if(s.size() != t.size()){
+                return false;
         }
-        
-        for(int i = 0; i < len; i++) { 
-            if(indexS[s[i]] != indexT[t[i]]) { 
-                return false; 
-            }
+        for(int i=0; i<s.size(); i++){
+            if(indexS[s[i]] != indexT[t[i]]) return false;
             
-            indexS[s[i]] = i + 1; 
-            indexT[t[i]] = i + 1;
+            indexS[s[i]] = i+1;
+            indexT[t[i]] = i+1;
+            
         }
-        
         return true;
     }
 };
