2c795a1474a52e1fec62cbb2373106c4ef201b07
Aarti <143300846+aartijangra@users.noreply.github.com>
2024-08-16 23:33:49 +0530

    Time: 4 ms (12.16%), Space: 8.1 MB (22.11%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,39 +1,11 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-//         int len = s.length();
-//         for(int i=0; i<len; i++){
-//             s+=s[i];
-//             string sub = s.substr(i+1, len+1);
-//             if(sub == goal) return true;
-            
-//         }
-//         return false;
-        
-        if(s.size()!=goal.size()) return false;
-        string concatenated = goal + goal;
         int n = s.size();
-        int m = concatenated.size();
-        int i=0; 
-        int j=0;
-        
-        while(i<m){
-            if(concatenated[i] == s[j]){
-                j++;
-                i++;
-                
-                if(j==n) return true;
-            }
-            
-            else{
-                if(j!=0){
-                    i=i-j+1;
-                    j=0;
-                }
-                else{
-                    i++;
-                }
-            }
+        for(int i=0; i<n; i++){
+            s+=s[i];
+            string sub = s.substr(i+1, n+1);
+            if(sub==goal) return true;
         }
         return false;
     }
