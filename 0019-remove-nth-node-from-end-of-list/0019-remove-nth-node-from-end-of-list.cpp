88c39bcc78e5313f357640698e89d77915f1d064
Aarti <143300846+aartijangra@users.noreply.github.com>
2024-08-24 01:06:40 +0530

    Time: 8 ms (12.51%), Space: 14.6 MB (65.35%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,27 +11,46 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* temp = head;
-        int cnt = 0;
-        while(temp){
-            cnt++;
-            temp = temp->next;
+        // ListNode* temp = head;
+        // int cnt = 0;
+        // while(temp){
+        //     cnt++;
+        //     temp = temp->next;
+        // }
+        // if(cnt == n){
+        //     ListNode* newHead = head->next;
+        //     return newHead;
+        //     free (head);
+        // }
+        // int res = cnt - n;
+        // temp = head;
+        // while(temp){
+        //     res--;
+        //     if(res==0) break;
+        //     else temp=temp->next;
+        // }
+        // temp->next = temp->next->next;
+        // ListNode* delNode = temp->next;
+        // // free (delNode);
+        // return head;
+        
+        ListNode* fast = head;
+        for(int i=0; i<n; i++) fast = fast->next;
+        if(fast == NULL){ 
+            return head->next;
+            free(head);
         }
-        if(cnt == n){
-            ListNode* newHead = head->next;
-            return newHead;
-            free (head);
+        
+        ListNode* slow = head;
+        
+        while(fast->next ){
+            slow = slow->next;
+            fast = fast->next;
         }
-        int res = cnt - n;
-        temp = head;
-        while(temp){
-            res--;
-            if(res==0) break;
-            else temp=temp->next;
-        }
-        temp->next = temp->next->next;
-        ListNode* delNode = temp->next;
-        // free (delNode);
+        
+        ListNode* del = slow->next;
+        slow->next = slow->next->next;
+        delete del;
         return head;
     }
 };
