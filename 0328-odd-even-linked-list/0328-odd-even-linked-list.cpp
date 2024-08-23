deddab25b1ce9df94b9699a4ffa1ec8c7be6f66e
Aarti <143300846+aartijangra@users.noreply.github.com>
2024-08-23 23:40:24 +0530

    Time: 4 ms (92.07%), Space: 15.3 MB (66.59%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -11,28 +11,43 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
+//         if(!head || !head->next) return head;
+//         vector<int> v;
+//         ListNode* temp = head;
+//         while(temp && temp->next){
+//             v.push_back(temp->val);
+//             temp=temp->next->next;
+//         }
+//         if(temp) v.push_back(temp->val);
+        
+//         temp = head->next;
+//         while(temp && temp->next){
+//             v.push_back(temp->val);
+//             temp=temp->next->next;
+//         }
+//         if(temp) v.push_back(temp->val);
+        
+//         temp = head; int i = 0;
+//         while(temp){
+//             temp->val = v[i];
+//             i++;
+//             temp = temp->next;
+//         }
+//         return head;
+        
+        
         if(!head || !head->next) return head;
-        vector<int> v;
-        ListNode* temp = head;
-        while(temp && temp->next){
-            v.push_back(temp->val);
-            temp=temp->next->next;
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evenHead = head->next;
+        while(even && even->next){
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+            
+            odd = odd->next;
+            even = even->next;
         }
-        if(temp) v.push_back(temp->val);
-        
-        temp = head->next;
-        while(temp && temp->next){
-            v.push_back(temp->val);
-            temp=temp->next->next;
-        }
-        if(temp) v.push_back(temp->val);
-        
-        temp = head; int i = 0;
-        while(temp){
-            temp->val = v[i];
-            i++;
-            temp = temp->next;
-        }
+        odd->next = evenHead;
         return head;
     }
 };
