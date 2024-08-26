bae1a93dc03bffbb3effe98694576332beaa65b5
Aarti <143300846+aartijangra@users.noreply.github.com>
2024-08-26 10:10:09 +0530

    Time: 3 ms (66.56%), Space: 7.6 MB (10.14%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -6,12 +6,21 @@ public:
 //         return fib(n-1)+fib(n-2);
         
         //memoization
+//         if(n<=1) return n;
+//         vector<int> dp(n+1, -1);
+        
+//         if(dp[n] != -1) return dp[n];
+        
+//         return dp[n] = fib(n-1) + fib(n-2);
+        
+        //tabulation
+        vector<int> dp(n+1);
         if(n<=1) return n;
-        vector<int> dp(n+1, -1);
-        
-        if(dp[n] != -1) return dp[n];
-        
-        return dp[n] = fib(n-1) + fib(n-2);
-        
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
