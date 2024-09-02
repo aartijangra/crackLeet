6b5c67e931e98995e53962ad8f8778308c850b81
Aarti <143300846+aartijangra@users.noreply.github.com>
2024-09-02 18:13:35 +0530

    Time: 0 ms (100.00%), Space: 14.9 MB (47.36%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,34 +1,7 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
-        //finding dip index
-        int ind = -1;
-        for(int i=n-2; i>=0; i--){
-            if(nums[i]<nums[i+1]){
-                ind = i;
-                break;
-            }
-        }
-        
-        //edge case
-        if(ind==-1){
-            reverse(nums.begin(),nums.end());
-            
-        }
-        
-        
-        //swap and then reverse remaining array
-        else{
-        for(int i=n-1; i>=ind; i--){
-            if(nums[i]>nums[ind]){
-                swap(nums[i],nums[ind]);
-                break;
-            }
-        }
-        reverse(nums.begin()+ind+1,nums.end());
-        
-        }
-        
+      
+        next_permutation(nums.begin(), nums.end());
     }
 };
